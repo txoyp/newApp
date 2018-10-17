2530e5d7ef9245238bd9ca602a40ad31abadf1c6
“txoyp <1401741458@qq.com>
2018-10-17 22:39:20 +0800

    10-17

--- src/views/blog/OwnPage.vue
+++ src/views/blog/OwnPage.vue
@@ -2,7 +2,17 @@
     <div class="blog">
         <div id="background">
             <div class="bg-head-img"></div>
-            <sapn>昵称</sapn>
+            <span>昵称</span>
+            <div class="bg-attention">
+                <div class="bg-fans-1">
+                    关注：100
+                </div>
+                <div class="bg-fans">
+                    粉丝：200
+                </div>
+            </div>
+
+            <div class="introduction">简介内容</div>
         </div>
         <div class="nav">
             <div class="nav-detail">
@@ -39,8 +49,59 @@
         height:100%;
     }
     #background{
-       height:30%;
-       width: 100%;
+        background:url("component/img/bg.png") center no-repeat;
+        background-size: 400px;
+        height:30%;
+        width: 100%;
+        color:#ffffff;
+        position:relative;
+        & .bg-head-img{
+            background: url("component/img/head.png");
+            position: absolute;
+            top: 30%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            height:66px;
+            width:66px;
+            border:1px solid #000;
+            border-radius: 10px;
+        }
+        span{
+            position: absolute;
+            top: 60%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            font-size: 16px;
+        }
+        & .bg-attention{
+            font-size: 14px;
+            height:24px;
+            width: 180px;
+            position: absolute;
+            top: 76%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+
+            & .bg-fans-1{
+                float: left;
+                height:24px;
+                width: 90px;
+                border-right: 1px solid #000;
+                padding: 0px 10px;
+            }
+            & .bg-fans{
+                position: absolute;
+                left:50%;
+                height:24px;
+                width: 90px;
+                padding-left: 16px;
+            }
+        }
+        & .introduction{
+            position: absolute;
+            top: 84%;
+            left: 42%;
+        }
     }
     .nav{
         height: 8%;
@@ -81,6 +142,5 @@
     #component{
         height: 56%;
         width: 100%;
-        border-bottom: 1px solid #ccc;
     }
 </style>
